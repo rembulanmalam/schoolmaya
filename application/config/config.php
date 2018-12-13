eaf79f12de730d93cd4c8cf2eb656f0e0e902f2c
ppyrinn <etoilefeexpress@yahoo.co.id>
2018-12-14 00:43:53 +0700

    fixed localhost

--- application/config/config.php
+++ application/config/config.php
@@ -15,7 +15,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 | If it is not set, then CodeIgniter will try guess the protocol and path
 | your installation, but due to security concerns the hostname will be set
-| to $_SERVER['SERVER_ADDR'] if available, or localhost:9080 otherwise.
+| to $_SERVER['SERVER_ADDR'] if available, or localhost otherwise.
 | The auto-detection mechanism exists only for convenience during
 | development and MUST NOT be used in production!
 |
